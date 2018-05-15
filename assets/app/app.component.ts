c22743b48d38c981aac4e3522cec2f8815290e63
Sai Vivek <vivek@Sais-MacBook-Pro.local>
2018-05-15 12:39:58 -0500

    add and delete message services created

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { Message } from './messages/message.model';
+import { MessageService } from './messages/message.service';
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styles: []
+    providers: [MessageService]
 })
 export class AppComponent {
-    message: Message = new Message('Some Message', 'Vivek');
+
 
 }
---
+++ assets/app/auth/authentication.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-authentication',
+    template: `
+    <h1>Authentication</h1>
+    `
+})
+export class AuthenticationComponent{
+
+}
