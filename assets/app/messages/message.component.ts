f186c0943f1184f54ccbd2d1ecf62160afb3c0d6
Sai Vivek <vivek@Sais-MacBook-Pro.local>
2018-05-15 20:06:04 -0500

    connected angular front end and node.js backend for creating,updating and deleting messages

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
-
+import { Component, Input } from "@angular/core";
 import { Message } from "./message.model";
 import { MessageService } from "./message.service";
+
 
 @Component({
     selector: 'app-message',
@@ -23,15 +23,17 @@ import { MessageService } from "./message.service";
 })
 export class MessageComponent {
     @Input() message: Message;
-    @Output() editClicked = new EventEmitter<string>();
 
     constructor(private messageService: MessageService) {}
 
     onEdit() {
-        this.editClicked.emit('A new value');
+       this.messageService.editMessage(this.message);
     }
 
     onDelete() {
-        this.messageService.deleteMessage(this.message);
+        this.messageService.deleteMessage(this.message)
+        .subscribe( 
+            result => console.log(result)
+        );
     }
 }
--- models/message.js
+++ models/message.js
@@ -1,10 +1,9 @@
-import { mongo } from 'mongoose';
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-    content: {type: String, required=true},
+    content: {type: String, required:true},
     user : {type: Schema.Types.ObjectId, ref:'User'}
 });
 
