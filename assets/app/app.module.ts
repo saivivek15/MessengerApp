7854e582135f580165005d8f54b9c05096dd6afc
Sai Vivek <vivek@Sais-MacBook-Pro.local>
2018-05-27 11:32:01 -0500

    seperate message module created

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from "./app.component";
-import { MessageComponent } from './messages/message.component';
-import { MessageListComponent } from './messages/message-list.component';
-import { MessageInputComponent } from './messages/message-input.component';
-import { MessagesComponent } from './messages/messages.component';
 import { AuthenticationComponent } from './auth/authentication.component';
 import { HeaderComponent } from './header.component';
 import { routing } from './app.routing';
@@ -17,15 +13,12 @@ import { SigninComponent } from './auth/signin.component';
 import { AuthService } from './auth/auth.service';
 import { ErrorComponent } from './errors/error.component';
 import { ErrorService } from './errors/error.service';
+import { MessageModule } from './messages/message.module';
 
 
 @NgModule({
     declarations: [
         AppComponent,
-        MessageComponent,
-        MessageListComponent,
-        MessageInputComponent,
-        MessagesComponent,
         AuthenticationComponent,
         HeaderComponent,
         LogoutComponent,
@@ -35,10 +28,11 @@ import { ErrorService } from './errors/error.service';
         ErrorComponent
     ],
     imports: [BrowserModule, 
-              FormsModule,
               routing,
               ReactiveFormsModule,
-              HttpModule],
+              HttpModule,
+            MessageModule
+        ],
     providers: [AuthService, ErrorService],
     bootstrap: [AppComponent]
 })
