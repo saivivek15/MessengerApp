c45c7582885ca255c76c1ea9bac8f48ea3c6013c
Sai Vivek <vivek@Sais-MacBook-Pro.local>
2018-05-27 11:20:42 -0500

    error handling with modals

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -15,6 +15,8 @@ import { LogoutComponent } from './auth/logout.component';
 import { SignupComponent } from './auth/signup.component';
 import { SigninComponent } from './auth/signin.component';
 import { AuthService } from './auth/auth.service';
+import { ErrorComponent } from './errors/error.component';
+import { ErrorService } from './errors/error.service';
 
 
 @NgModule({
@@ -29,14 +31,15 @@ import { AuthService } from './auth/auth.service';
         LogoutComponent,
         SignupComponent,
         SigninComponent,
-        LogoutComponent
+        LogoutComponent,
+        ErrorComponent
     ],
     imports: [BrowserModule, 
               FormsModule,
               routing,
               ReactiveFormsModule,
               HttpModule],
-    providers: [AuthService],
+    providers: [AuthService, ErrorService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
